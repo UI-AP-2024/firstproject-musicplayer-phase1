c8505a8c2483d6abea157a487cf45b072de873d6
Saghar <saghar.ghasemi.tabatabaie@gmail.com>
2024-03-27 18:04:31 +0330

    singleton constructor of class Admin

---
+++ Music Player/src/model/Admin.java
@@ -0,0 +1,20 @@
+package model;
+
+import java.time.LocalDate;
+
+public class Admin extends UserAccount
+{
+    private static Admin admin;
+    private Admin(String email, String name, String phoneNumber, LocalDate dateOfBirth, String userName, String passWord)
+    {
+        super(email,name,phoneNumber,dateOfBirth,userName,passWord);
+    }
+    public static synchronized Admin getAdmin(String email, String name, String phoneNumber, LocalDate dateOfBirth, String userName, String passWord)
+    {
+        if (admin == null)
+        {
+            admin = new Admin(email,name,phoneNumber,dateOfBirth,userName,passWord);
+        }
+        return admin;
+    }
+}
