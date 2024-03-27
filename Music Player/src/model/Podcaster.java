bf5aabcc61c2c5d3345bf0fc440cca63d5661aea
Saghar <saghar.ghasemi.tabatabaie@gmail.com>
2024-03-27 20:24:09 +0330

    array list az jens Podcast va constructor

--- Music Player/src/model/Podcaster.java
+++ Music Player/src/model/Podcaster.java
@@ -1,9 +1,12 @@
 package model;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 
 public class Podcaster extends Artist{
-    public Podcaster(String email, String name, String phoneNumber, LocalDate dateOfBirth, String userName, String passWord) {
-        super(email, name, phoneNumber, dateOfBirth, userName, passWord);
+
+    ArrayList<Podcast> podcasts = new ArrayList<Podcast>();
+    public Podcaster(String email, String name, String phoneNumber, LocalDate dateOfBirth, String userName, String passWord, String biography) {
+        super(email, name, phoneNumber, dateOfBirth, userName, passWord, biography);
     }
 }
