3411a0a3bf92d9abcc37585179231ccef6a24c2b
Sina Mohammadizadeh <sinamhz0407@gmail.com>
2024-03-27 18:52:18 +0330

    YY

---
+++ MusicPlayer/src/Model/User.java
@@ -0,0 +1,66 @@
+package Model;
+import java.util.*;
+abstract public class User {
+    private String username;
+    private String password;
+    private String fullName;
+    private String email;
+    private String phoneNumber;
+    private Date birthDate;
+    public User(String username, String password, String fullName, String email, String phoneNumber, Date birthDate) {
+        this.username = username;
+        this.password = password;
+        this.fullName = fullName;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.birthDate = birthDate;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+    }
+}
