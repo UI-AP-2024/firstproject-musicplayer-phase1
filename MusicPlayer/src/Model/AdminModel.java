e167c2cf7c039c7271d61f2771eb7e4f597dc02d
Sina Mohammadizadeh <sinamhz0407@gmail.com>
2024-03-27 21:10:49 +0330

    YYY

---
+++ MusicPlayer/src/Model/AdminModel.java
@@ -0,0 +1,18 @@
+package Model;
+
+import java.util.Date;
+
+public class AdminModel extends User{
+    private static AdminModel admin;
+    private AdminModel(String username, String password, String fullName, String email, String phoneNumber, Date birthDate){
+        super(username,password,fullName,email,phoneNumber,birthDate);
+    }
+    public AdminModel getAdmin(String username, String password, String fullName, String email, String phoneNumber, Date birthDate){
+        if(admin==null)
+            admin = new AdminModel(username,password,fullName,email,phoneNumber,birthDate);
+        return admin;
+    }
+    public AdminModel getAdmin(){
+        return admin;
+    }
+}
