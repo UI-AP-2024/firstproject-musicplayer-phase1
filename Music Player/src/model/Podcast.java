793900d90dada9dcb9ea4de764e6d9ec85d51e30
Saghar <saghar.ghasemi.tabatabaie@gmail.com>
2024-03-27 23:08:33 +0330

    String caption, getter , setter , constructor for class Podcast

--- Music Player/src/model/Podcast.java
+++ Music Player/src/model/Podcast.java
@@ -1,6 +1,18 @@
 package model;
 
+import java.time.LocalDate;
+
 public abstract class Podcast extends Audio
 {
-
+    private String caption;
+    public Podcast(String artistName, int year, int month, int day, LocalDate dateOfRelease, Genre genre,String caption, String name, String audioLink, String cover) {
+        super(artistName, year, month, day, dateOfRelease, genre, name, audioLink, cover);
+        this.caption = caption;
+    }
+    public String getCaption() {
+        return caption;
+    }
+    public void setCaption(String caption) {
+        this.caption = caption;
+    }
 }
