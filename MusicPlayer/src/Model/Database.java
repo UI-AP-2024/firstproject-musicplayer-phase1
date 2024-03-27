4089eed17a1dfab878d9d5d977f3dadc3429f3e1
Sina Mohammadizadeh <sinamhz0407@gmail.com>
2024-03-27 12:15:00 +0330

    s

--- MusicPlayer/src/Model/Database.java
+++ MusicPlayer/src/Model/Database.java
@@ -1,4 +1,12 @@
 package Model;
 
 public class Database {
+    private static Database database;
+    private Database(){
+    }
+    public static Database getDatabase(){
+        if(database==null)
+            database = new Database();
+        return database;
+    }
 }
