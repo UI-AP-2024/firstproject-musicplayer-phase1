cc53eb2be2f077e0959910973509e632b6be6d03
Saghar <saghar.ghasemi.tabatabaie@gmail.com>
2024-03-27 20:25:43 +0330

    array list az jens Album va constructor

--- Music Player/src/model/Singer.java
+++ Music Player/src/model/Singer.java
@@ -1,8 +1,10 @@
 package model;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 
 public class Singer extends Artist{
+    ArrayList<Album> albums = new ArrayList<Album>();
     public Singer(String email, String name, String phoneNumber, LocalDate dateOfBirth, String userName, String passWord, String biography) {
         super(email, name, phoneNumber, dateOfBirth, userName, passWord, biography);
     }
