45806e260557fa7698a1d25c8f8c70b09a7fc2c7
Saghar <saghar.ghasemi.tabatabaie@gmail.com>
2024-03-28 09:44:22 +0330

    ArrayList of Podcast class,getter,setter, constructor for class Podcaster

--- Music Player/src/model/Podcaster.java
+++ Music Player/src/model/Podcaster.java
@@ -5,8 +5,16 @@ import java.util.ArrayList;
 
 public class Podcaster extends Artist{
 
-    ArrayList<Podcast> podcasts = new ArrayList<Podcast>();
+    private ArrayList<Podcast> podcasts = new ArrayList<Podcast>();
     public Podcaster(String email, String name, String phoneNumber, LocalDate dateOfBirth, String userName, String passWord, String biography) {
         super(email, name, phoneNumber, dateOfBirth, userName, passWord, biography);
+    }
+
+    public ArrayList<Podcast> getPodcasts() {
+        return podcasts;
+    }
+
+    public void setPodcasts(ArrayList<Podcast> podcasts) {
+        this.podcasts = podcasts;
     }
 }
