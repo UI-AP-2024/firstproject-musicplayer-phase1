b4338125c27c4a195fac33863bec2d4137e5a620
Saghar <saghar.ghasemi.tabatabaie@gmail.com>
2024-03-28 09:45:37 +0330

    ArrayList of Album class,getter,setter, constructor for class Singer

--- Music Player/src/model/Singer.java
+++ Music Player/src/model/Singer.java
@@ -4,8 +4,16 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 
 public class Singer extends Artist{
-    ArrayList<Album> albums = new ArrayList<Album>();
+    private ArrayList<Album> albums = new ArrayList<Album>();
     public Singer(String email, String name, String phoneNumber, LocalDate dateOfBirth, String userName, String passWord, String biography) {
         super(email, name, phoneNumber, dateOfBirth, userName, passWord, biography);
+    }
+
+    public ArrayList<Album> getAlbums() {
+        return albums;
+    }
+
+    public void setAlbums(ArrayList<Album> albums) {
+        this.albums = albums;
     }
 }
