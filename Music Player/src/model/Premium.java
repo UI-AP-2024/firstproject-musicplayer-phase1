dba635db807c60d16264169aeb54d02ecf6af9dc
Saghar <saghar.ghasemi.tabatabaie@gmail.com>
2024-03-28 07:47:48 +0330

    fields,getter,setter, constructor for class Premium

--- Music Player/src/model/Premium.java
+++ Music Player/src/model/Premium.java
@@ -2,8 +2,18 @@ package model;
 
 import java.time.LocalDate;
 
-public class Premium extends Listener{
-    public Premium(String email, String name, String phoneNumber, LocalDate dateOfBirth, String userName, String passWord) {
-        super(email, name, phoneNumber, dateOfBirth, userName, passWord);
+public class Premium extends Listener
+{
+    private int remainingDays;
+    public Premium(String email, String name, String phoneNumber, LocalDate dateOfBirth, String userName, String passWord, Genre[] genres) {
+        super(email, name, phoneNumber, dateOfBirth, userName, passWord, genres);
+    }
+
+    public int getRemainingDays() {
+        return remainingDays;
+    }
+
+    public void setRemainingDays(int remainingDays) {
+        this.remainingDays = remainingDays;
     }
 }
