64c8a7613cc3cced4471b4c53c7edf0662ca4833
Sina Mohammadizadeh <sinamhz0407@gmail.com>
2024-03-29 00:58:29 +0330

    Y

---
+++ MusicPlayer/src/Controller/ListenerController.java
@@ -0,0 +1,38 @@
+package Controller;
+import Model.*;
+
+import java.util.Date;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class ListenerController extends User{
+    private FreeModel model;
+    public ListenerController(String username, String password, String fullName, String email, String phoneNumber, Date birthDate) {
+        super(username, password, fullName, email, phoneNumber, birthDate);
+    }
+    public String signUp(String username, String password, String fullName, String email, String phoneNumber, Date birthDate){
+        for (int i = 0; i < Database.getDatabase().getUsers().size(); i++) {
+            if(Database.getDatabase().getUsers().get(i).getUsername().equals(username)){
+                return "This Username already used!";
+            }
+        }
+        Pattern emailPattern = Pattern.compile(".*@.*(\\.com|\\.ir|\\.org)$");
+        Matcher matcherEmail = emailPattern.matcher(email);
+        Pattern phonepattern = Pattern.compile("^\\d{6,11}$");
+        Matcher matcherPhone = phonepattern.matcher(phoneNumber);
+        if(!matcherEmail.matches())
+            return "The email is not valid!";
+        else if(!matcherPhone.matches())
+            return "The phone number is not valid!";
+        model = new FreeModel(username, password, fullName, email, phoneNumber, birthDate);
+
+        return "Done!";
+    }
+    public void selectGenres(Genre genre){
+        int count=0;
+        while(genre!=null && count < 4){
+            model.getFavoriteGenres()[count] = genre;
+            count++;
+        }
+    }
+}
