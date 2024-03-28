d065e90b5883dff505d497b6e1b1db55080dbeb3
Saghar <saghar.ghasemi.tabatabaie@gmail.com>
2024-03-28 07:51:37 +0330

    fields,getter,setter, constructor for class Report

--- Music Player/src/model/Report.java
+++ Music Player/src/model/Report.java
@@ -1,4 +1,32 @@
 package model;
 
-public class Report {
+public class Report
+{
+    private String reportingUser;
+    private String reportedArtist;
+    private String description;
+    public Report(String reportingUser, String reportedArtist, String description)
+    {
+        this.reportingUser = reportingUser;
+        this.reportedArtist = reportedArtist;
+        this.description = description;
+    }
+    public String getReportingUser() {
+        return reportingUser;
+    }
+    public void setReportingUser(String reportingUser) {
+        this.reportingUser = reportingUser;
+    }
+    public String getReportedArtist() {
+        return reportedArtist;
+    }
+    public void setReportedArtist(String reportedArtist) {
+        this.reportedArtist = reportedArtist;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
