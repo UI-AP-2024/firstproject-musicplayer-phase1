ebe67e38b54753b593233c451c7e4de99d39b9f5
Saghar <saghar.ghasemi.tabatabaie@gmail.com>
2024-03-28 15:59:42 +0330

    fields,ArrayList of Playlist class,map,getter,setter, constructor for class Listener

--- Music Player/src/model/Listener.java
+++ Music Player/src/model/Listener.java
@@ -1,10 +1,59 @@
 package model;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Listener extends UserAccount
 {
-    public Listener(String email, String name, String phoneNumber, LocalDate dateOfBirth, String userName, String passWord) {
+    private int accountCredit = 50;
+    private Date expirationDate = null;
+    private Genre[] genres = new Genre[4];
+    private ArrayList<Playlist> playlists = new ArrayList<Playlist>();
+    private Map<String,Integer> map = new HashMap<>();
+    public Listener(String email, String name, String phoneNumber, LocalDate dateOfBirth, String userName, String passWord,Genre[] genres) {
         super(email, name, phoneNumber, dateOfBirth, userName, passWord);
+        this.genres = genres;
+    }
+    public int getAccountCredit() {
+        return accountCredit;
+    }
+
+    public void setAccountCredit(int accountCredit) {
+        this.accountCredit = accountCredit;
+    }
+
+    public Date getExpirationDate() {
+        return expirationDate;
+    }
+
+    public void setExpirationDate(Date expirationDate) {
+        this.expirationDate = expirationDate;
+    }
+
+    public Genre[] getGenres() {
+        return genres;
+    }
+
+    public void setGenres(Genre[] genres) {
+        this.genres = genres;
+    }
+
+    public ArrayList<Playlist> getPlaylists() {
+        return playlists;
+    }
+
+    public void setPlaylists(ArrayList<Playlist> playlists) {
+        this.playlists = playlists;
+    }
+
+    public Map<String, Integer> getMap() {
+        return map;
+    }
+
+    public void setMap(Map<String, Integer> map) {
+        this.map = map;
     }
 }
