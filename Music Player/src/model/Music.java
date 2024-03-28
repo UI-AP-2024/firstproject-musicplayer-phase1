d2702fc32bc67bd8330864463b7f0155e8d77779
Saghar <saghar.ghasemi.tabatabaie@gmail.com>
2024-03-28 16:04:33 +0330

    String lyrics,getter,setter, constructor for class Music

--- Music Player/src/model/Music.java
+++ Music Player/src/model/Music.java
@@ -1,5 +1,17 @@
 package model;
 
+import java.time.LocalDate;
+
 public abstract class Music extends Audio
 {
+    private String lyrics;
+    public Music(String artistName, int year, int month, int day, LocalDate dateOfRelease, Genre genre, String name, String audioLink, String cover) {
+        super(artistName, year, month, day, dateOfRelease, genre, name, audioLink, cover);
+    }
+    public String getLyrics() {
+        return lyrics;
+    }
+    public void setLyrics(String lyrics) {
+        this.lyrics = lyrics;
+    }
 }
