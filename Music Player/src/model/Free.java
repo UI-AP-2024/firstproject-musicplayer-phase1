39cc6640be723261ad503335e7ec62c42eae015d
Saghar <saghar.ghasemi.tabatabaie@gmail.com>
2024-03-28 16:14:28 +0330

    ArrayList of Playlist with capacity,ArrayList of Audio with capacity,getter,setter, constructor for class Free

--- Music Player/src/model/Free.java
+++ Music Player/src/model/Free.java
@@ -1,9 +1,20 @@
 package model;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 
-public class Free extends Listener{
-    public Free(String email, String name, String phoneNumber, LocalDate dateOfBirth, String userName, String passWord) {
-        super(email, name, phoneNumber, dateOfBirth, userName, passWord);
+public class Free extends Listener
+{
+    private final ArrayList<Playlist> playlists = new ArrayList<Playlist>(3);
+    private final ArrayList<Audio> audio = new ArrayList<Audio>(10);
+    public Free(String email, String name, String phoneNumber, LocalDate dateOfBirth, String userName, String passWord, Genre[] genres) {
+        super(email, name, phoneNumber, dateOfBirth, userName, passWord, genres);
+    }
+    @Override
+    public ArrayList<Playlist> getPlaylists() {
+        return playlists;
+    }
+    public ArrayList<Audio> getAudio() {
+        return audio;
     }
 }
