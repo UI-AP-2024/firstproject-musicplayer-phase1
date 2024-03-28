68fad3c8fc956e4853b7c679241f5947535ac3f5
Saghar <saghar.ghasemi.tabatabaie@gmail.com>
2024-03-28 05:56:48 +0330

    fields,getter,setter, constructor for class Album

--- Music Player/src/model/Album.java
+++ Music Player/src/model/Album.java
@@ -1,4 +1,49 @@
 package model;
 
-public class Album {
+import java.util.ArrayList;
+
+public class Album
+{
+    private int j = 0;
+    private String name;
+    private String artistName;
+    ArrayList<Audio> album = new ArrayList<Audio>();
+
+    public Album(String name, String artistName) {
+        this.name = name;
+        this.artistName = artistName;
+        j = ++j;
+    }
+
+    public int getJ() {
+        return j;
+    }
+
+    public void setJ(int j) {
+        this.j = j;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getArtistName() {
+        return artistName;
+    }
+
+    public void setArtistName(String artistName) {
+        this.artistName = artistName;
+    }
+
+    public ArrayList<Audio> getAlbum() {
+        return album;
+    }
+
+    public void setAlbum(ArrayList<Audio> album) {
+        this.album = album;
+    }
 }
