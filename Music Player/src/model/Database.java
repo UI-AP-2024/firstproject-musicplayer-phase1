a067c7ea1af66f6c5660860afa186b28de240cf8
Saghar <saghar.ghasemi.tabatabaie@gmail.com>
2024-03-28 16:22:27 +0330

    ArrayLists of UserAccount and Audio and Report,getter,setter, constructor for class Database

--- Music Player/src/model/Database.java
+++ Music Player/src/model/Database.java
@@ -1,6 +1,49 @@
 package model;
+import model.*;
+import java.util.ArrayList;
 
 public class Database
 {
-    //singleton
+    private ArrayList<UserAccount> userAccounts = new ArrayList<UserAccount>();
+    private ArrayList<Audio> audio = new ArrayList<Audio>();
+    private ArrayList<Report> reports = new ArrayList<Report>();
+    private static Database database;
+    private Database(ArrayList<UserAccount> userAccounts, ArrayList<Audio> audio, ArrayList<Report> reports)
+    {
+        this.userAccounts = userAccounts;
+        this.audio = audio;
+        this.reports = reports;
+    }
+    public static Database getDatabase(ArrayList<UserAccount> userAccounts, ArrayList<Audio> audio, ArrayList<Report> reports)
+    {
+        if(database == null)
+        {
+            database = new Database(userAccounts,audio,reports);
+        }
+        return database;
+    }
+    public ArrayList<UserAccount> getUserAccounts() {
+        return userAccounts;
+    }
+    public void setUserAccounts(ArrayList<UserAccount> userAccounts) {
+        this.userAccounts = userAccounts;
+    }
+    public ArrayList<Audio> getAudio() {
+        return audio;
+    }
+    public void setAudio(ArrayList<Audio> audio) {
+        this.audio = audio;
+    }
+    public ArrayList<Report> getReports() {
+        return reports;
+    }
+    public void setReports(ArrayList<Report> reports) {
+        this.reports = reports;
+    }
+    public static Database getDatabase() {
+        return database;
+    }
+    public static void setDatabase(Database database) {
+        Database.database = database;
+    }
 }
