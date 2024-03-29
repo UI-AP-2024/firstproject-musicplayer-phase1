9180d4409a1efcf6ec3dae3b621648ba3d4335df
mohsen <im.mohsen.369@gmail.com>
2024-03-30 00:44:49 +0430

    creating dataBase...

--- mpproj/src/model/DataBase/DataBase.java
+++ mpproj/src/model/DataBase/DataBase.java
@@ -1,5 +1,41 @@
-package model.Database;
+package model.DataBase;
 
-public class Database {
-    
+import java.util.ArrayList;
+
+public class DataBase {
+
+    private ArrayList<Users> users;
+    private ArrayList<Aoudios> aoudios;
+    private ArrayList<Reports> reports;
+    private static DataBase dataBase;
+
+    public static DataBase getDataBase() {
+        if (dataBase == null)
+            dataBase = new DataBase();
+        return dataBase;
+    }
+
+    public ArrayList<Users> getUsers() {
+        return users;
+    }
+
+    public ArrayList<Aoudios> getAoudios() {
+        return aoudios;
+    }
+
+    public ArrayList<Reports> getReports() {
+        return reports;
+    }
+
+    public void setUsers(ArrayList<Users> users) {
+        this.users = users;
+    }
+
+    public void setAoudios(ArrayList<Aoudios> aoudios) {
+        this.aoudios = aoudios;
+    }
+
+    public void setReports(ArrayList<Reports> reports) {
+        this.reports = reports;
+    }
 }
