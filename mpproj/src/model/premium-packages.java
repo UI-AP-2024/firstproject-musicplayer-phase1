303791b23690c0328092b71cc57e28b644fd8090
mohsen <im.mohsen.369@gmail.com>
2024-03-29 23:01:15 +0430

    rewritePremiumEnum

--- mpproj/src/model/premium-packages.java
+++ mpproj/src/model/premium-packages.java
@@ -1,11 +1,18 @@
 package model;
 public enum PremiumPackages {
-    THERTEENDAYS(5), SIXTEENDAYS(9), ONEHUNDREDANDEIGHTEENDAYS(14);
+    FIRST(5, 30), SECOND(9, 60), THIRD(14, 180);
 
     private final int cash;
+    private final int days;
 
-    PremiumPackages(int cash) {
+
+    PremiumPackages(int cash, int days) {
         this.cash = cash;
+        this.days = days;
+    }
+
+    private int getDays() {
+        return this.days;
     }
 
     public int getCash() {
