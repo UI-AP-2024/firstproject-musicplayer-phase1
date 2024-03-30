fb6a914d6e30d3236d8d0d4ec055b1e085a058af
Alireza <aliharadi10@gmail.com>
2024-03-31 02:17:51 +0330

    creating the UserAccount class in model pkg

---
+++ mpproj/src/model/UserAccount.java
@@ -0,0 +1,12 @@
+package model;
+
+import java.util.Date;
+
+public abstract class UserAccount {
+    String userName;
+    String password;
+    String name;
+    String email;
+    String phoneNumber;
+    Date dateOfBirth;
+}
