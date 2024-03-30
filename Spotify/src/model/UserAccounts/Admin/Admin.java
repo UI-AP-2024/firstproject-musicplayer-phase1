5ca24f9c0bef135d13be04f56e667eb9c4f75b48
AliMirlohi <ali84mirloho@gmail.com>
2024-03-31 00:21:46 +0330

    complete Admin.java model;

--- Spotify/src/model/UserAccounts/Admin/Admin.java
+++ Spotify/src/model/UserAccounts/Admin/Admin.java
@@ -2,5 +2,21 @@ package model.UserAccounts.Admin;
 
 import model.UserAccounts.userAccount;
 
+import java.util.Date;
+
 public class Admin extends userAccount {
+    private Admin admin;
+    private Admin(String password, String fullName, String email, String phoneNumber, Date birthday) {
+        super(password, fullName, email, phoneNumber, birthday);
+    }
+
+    public Admin getAdmin(String password, String fullName, String email, String phoneNumber, Date birthday){
+        if (admin==null){
+            admin = new Admin(password, fullName, email, phoneNumber, birthday);
+        }
+        return admin;
+    }
+    public Admin getAdmin(){
+        return admin;
+    }
 }
