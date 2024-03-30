371138d55a761ad98c9371d37f6de5c2f886591a
AliMirlohi <ali84mirloho@gmail.com>
2024-03-30 21:54:32 +0330

    complete Audio.java model;

--- Spotify/src/model/Audio/Audio.java
+++ Spotify/src/model/Audio/Audio.java
@@ -1,5 +1,119 @@
 package model.Audio;
-
+import java.util.Date;
+import model.Genre.Genre;
 public abstract class Audio {
+    private int audioId;
+    private static int audioIdCounter=0;
+    private String fileName;
+    private String artistName;
+    private int numberOfPlays;
+    private int numberOfLikes;
+    private Date dateOfRelease;
+    private Genre genre;
+    private String fileLink;
+    private String cover;
 
+    public Audio(String fileName, String artistName, Date dateOfRelease, Genre genre, String fileLink, String cover) {
+        this.audioId=++audioIdCounter;
+        this.fileName = fileName;
+        this.artistName = artistName;
+        this.numberOfPlays = 0;
+        this.numberOfLikes=0;
+        this.dateOfRelease = dateOfRelease;
+        this.genre = genre;
+        this.fileLink = fileLink;
+        this.cover = cover;
+    }
+    @Override
+    public String toString(){
+        StringBuilder context=new StringBuilder();
+        context.append("audio id : ");
+        context.append(audioId);
+        context.append("\naudio name : ");
+        context.append(fileName);
+        context.append("\nartist name : ");
+        context.append(artistName);
+        context.append("\nnumber of plays : ");
+        context.append(numberOfPlays);
+        context.append("\nnumber of likes : ");
+        context.append(numberOfLikes);
+        context.append("\ndate of release : ");
+        context.append(dateOfRelease.getYear()+1900);
+        context.append("/");
+        context.append(dateOfRelease.getMonth()+1);
+        context.append("/");
+        context.append(dateOfRelease.getDate());
+        return context.toString();
+    }
+
+    public int getAudioId() {
+        return audioId;
+    }
+
+    public int getAudioIdCounter(){
+        return audioIdCounter;
+    }
+    public String getFileName() {
+        return fileName;
+    }
+
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+
+    public String getArtistName() {
+        return artistName;
+    }
+
+    public void setArtistName(String artistName) {
+        this.artistName = artistName;
+    }
+
+    public int getNumberOfPlays() {
+        return numberOfPlays;
+    }
+
+    public void setNumberOfPlays(int numberOfPlays) {
+        this.numberOfPlays = numberOfPlays;
+    }
+
+    public int getNumberOfLikes() {
+        return numberOfLikes;
+    }
+
+    public void setNumberOfLikes(int numberOfLikes) {
+        this.numberOfLikes = numberOfLikes;
+    }
+
+    public Date getDateOfRelease() {
+        return dateOfRelease;
+    }
+
+    public void setDateOfRelease(Date dateOfRelease) {
+        this.dateOfRelease = dateOfRelease;
+    }
+
+    public Genre getGenre() {
+        return genre;
+    }
+
+    public void setGenre(Genre genre) {
+        this.genre = genre;
+    }
+
+    public String getFileLink() {
+        return fileLink;
+    }
+
+    public void setFileLink(String fileLink) {
+        this.fileLink = fileLink;
+    }
+
+    public String getCover() {
+        return cover;
+    }
+
+    public void setCover(String cover) {
+        this.cover = cover;
+    }
 }
