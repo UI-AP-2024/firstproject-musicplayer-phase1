68c13f0ceaa62a1abbbaffc46ab12e249592be5e
mohsen <im.mohsen.369@gmail.com>
2024-03-31 00:54:47 +0430

    completing album class...

--- mpproj/src/model/Audio/Music.java
+++ mpproj/src/model/Audio/Music.java
@@ -1,4 +1,18 @@
 package model.Audio;
 
-public class Music {
+import model.Genre;
+
+import java.util.Date;
+
+public class Music extends Audio{
+    private final String lyrics;
+
+    public Music(int id, String audioTitle, String artistName, Date releaseDate, Genre genre, String audioLink, String cover, String lyrics) {
+        super(id, audioTitle, artistName, releaseDate, genre, audioLink, cover);
+        this.lyrics = lyrics;
+    }
+
+    public String getLyrics() {
+        return lyrics;
+    }
 }
--- mpproj/src/model/Audio/Podcast.java
+++ mpproj/src/model/Audio/Podcast.java
@@ -1,4 +1,17 @@
 package model.Audio;
 
-public class Podcast {
+import model.Genre;
+
+import java.util.Date;
+
+public class Podcast extends Audio{
+    private final String caption;
+    public Podcast(int id, String audioTitle, String artistName, Date releaseDate, Genre genre, String audioLink, String cover, String caption) {
+        super(id, audioTitle, artistName, releaseDate, genre, audioLink, cover);
+        this.caption = caption;
+    }
+
+    public String getCaption() {
+        return caption;
+    }
 }
