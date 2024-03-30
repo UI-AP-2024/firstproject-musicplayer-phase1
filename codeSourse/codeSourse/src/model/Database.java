e6dd297aeece819020a264643136da7f475e342e
Elnazt27 <151857047+Elnazt27@users.noreply.github.com>
2024-03-30 20:09:19 +0330

    c01 Database class

---
+++ codeSourse/codeSourse/src/model/Database.java
@@ -0,0 +1,27 @@
+package model;
+
+import java.util.ArrayList;
+
+public class Database {
+    private static  Database instance;
+    private List<User> users;
+    private List<Audio> audiofiles;
+    private List<Report> reports;
+
+ //*********************************************
+   private Database(){
+       users = new ArrayList<>();
+       audiofiles = new ArrayList<>();
+       reports = new ArrayList<>();
+
+   }
+ //*********************************************
+
+public static Database getInstance(){
+       if(instance = null){
+           instance = new Database();
+       }
+       return instance;
+}
+
+}
