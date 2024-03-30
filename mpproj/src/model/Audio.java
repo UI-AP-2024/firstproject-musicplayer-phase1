82466186fa3d7cd58ca0295888af938df8103abe
Alireza <aliharadi10@gmail.com>
2024-03-31 01:47:35 +0330

    creating the packages and Audio class in model pkg

---
+++ mpproj/src/model/Audio.java
@@ -0,0 +1,16 @@
+package model;
+
+import java.util.Date;
+
+public abstract class Audio {
+    long numericalId;
+    String name;
+    String nameArtist;
+    int numberOfPolys;
+    int numberOfLikes;
+    Date DateOfRelease;
+    Gener gener;
+    String audioLink;
+    String cover;
+}
+
