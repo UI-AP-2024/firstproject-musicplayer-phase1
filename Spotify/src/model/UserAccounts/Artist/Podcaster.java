d9eeede4c926b9b0dbd3c1cda87940cfa666ecf8
AliMirlohi <ali84mirloho@gmail.com>
2024-03-31 00:39:07 +0330

    complete Podcaster.java model;

--- Spotify/src/model/UserAccounts/Artist/Podcaster.java
+++ Spotify/src/model/UserAccounts/Artist/Podcaster.java
@@ -1,4 +1,31 @@
 package model.UserAccounts.Artist;
+import model.Audio.Podcast;
+import model.UserAccounts.userAccount;
+
+import java.util.ArrayList;
+import java.util.Date;
 
 public class Podcaster extends Artist{
+    private ArrayList<Podcast> podcastsList;
+    public Podcaster(String password, String fullName, String email, String phoneNumber, Date birthday, double income, ArrayList<userAccount> followersList, String biograghy,ArrayList<Podcast> podcastsList) {
+        super(password, fullName, email, phoneNumber, birthday, income, followersList, biograghy);
+        this.podcastsList = podcastsList;
+    }
+
+    @Override
+    public String toString(){
+        StringBuilder context = new StringBuilder(super.toString());
+        context.append("\n");
+        for(Podcast podcast:podcastsList){
+            context.append(podcast.toString());
+        }
+        return context.toString();
+    }
+    public ArrayList<Podcast> getPodcastsList() {
+        return podcastsList;
+    }
+
+    public void setPodcastsList(ArrayList<Podcast> podcastsList) {
+        this.podcastsList = podcastsList;
+    }
 }
