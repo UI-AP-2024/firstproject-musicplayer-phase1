72e271489fc136632cf47ddef4e62514084cd73a
Elnazt27 <151857047+Elnazt27@users.noreply.github.com>
2024-03-31 19:21:30 +0330

    c14 edited Album.

--- codeSourse/codeSourse/src/model/Album.java
+++ codeSourse/codeSourse/src/model/Album.java
@@ -1,35 +1,60 @@
 package model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Album {
     private int uniqeId;
     private String name;
-
+    private String artistName;
+    private List<String> songs;
     //*********************************************
-    public Album(int uniqeId,String name){
+    public Album(int uniqeId, String name, String artistName) {
         this.uniqeId = uniqeId;
         this.name = name;
+        this.artistName = artistName;
+        this.songs = new ArrayList<>();
     }
     //*********************************************
-    public int getUniqeId(){
+    public int getUniqeId() {
         return uniqeId;
     }
-    public String getName(){
+
+    public String getName() {
         return name;
     }
+
+    public String getArtistName() {
+        return artistName;
+    }
+
+    public List<String> getSongs() {
+        return songs;
+    }
     //*********************************************
-    public void setUniqeId(int uniqeId){
+    public void setUniqeId(int uniqeId) {
         this.uniqeId = uniqeId;
     }
-    public void setName(String name){
+
+    public void setName(String name) {
         this.name = name;
     }
+
+    public void setArtistName(String artistName) {
+        this.artistName = artistName;
+    }
+
+    public void addSong(String songName) {
+        songs.add(songName);
+    }
     //*********************************************
-   @Override
-    public String toString(){
+    @Override
+    public String toString() {
         return "Album{" +
                 "uniqeId=" + uniqeId +
                 ", name='" + name + '\'' +
-                '}' ;
+                ", artistName='" + artistName + '\'' +
+                ", songs=" + songs +
+                '}';
     }
-
 }
