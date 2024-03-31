3dc47cb31933dfc6d8cfe5818d200079a0c76b56
Maryam Dar <maryamdar1384@gmail.com>
2024-03-31 18:06:27 +0330

    add album class

---
+++ mpproj/src/model/audio/Album.java
@@ -0,0 +1,47 @@
+package model.audio;
+
+import java.util.*;
+
+public class Album {
+    private final long id;
+    private String albumName;
+    private String artistName;
+    ArrayList<Music> musicList;
+    private static long albumCounter=0;
+    public Album(String albumName, String artistName) {
+        albumCounter++;
+        this.albumName = albumName;
+        this.artistName = artistName;
+        this.musicList = new ArrayList<>();
+        this.id=albumCounter;
+    }
+    public long getId() {
+        return id;
+    }
+    public String getAlbumName() {
+        return albumName;
+    }
+    public String getArtistName() {
+        return artistName;
+    }
+    public ArrayList<Music> getMusicList() {
+        return musicList;
+    }
+    public static long getAlbumCounter() {
+        return albumCounter;
+    }
+    public void setAlbumName(String albumName) {
+        this.albumName = albumName;
+    }
+    public void setArtistName(String artistName) {
+        this.artistName = artistName;
+    }
+    public void setMusicList(ArrayList<Music> musicList) {
+        this.musicList = musicList;
+    }
+    public static void setAlbumCounter(long albumCounter) {
+        Album.albumCounter = albumCounter;
+    }
+    
+    
+}
