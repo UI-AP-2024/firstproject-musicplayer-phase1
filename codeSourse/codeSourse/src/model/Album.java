91bfa0c1632fbf9352abd9eda31956207c621091
Elnazt27 <151857047+Elnazt27@users.noreply.github.com>
2024-03-31 10:51:32 +0330

    c05 Album class

---
+++ codeSourse/codeSourse/src/model/Album.java
@@ -0,0 +1,32 @@
+package model;
+
+public class Album {
+    private int uniqeId;
+    private String name;
+
+    //*********************************************
+    public Album(int uniqeId,String name){
+        this.uniqeId = uniqeId;
+        this.name = name;
+    }
+    //*********************************************
+    public int getUniqeId(){
+        return uniqeId;
+    }
+    public String getName(){
+        return name;
+    }
+    //*********************************************
+    public void setUniqeId(int uniqeId){
+        this.uniqeId = uniqeId;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+    //*********************************************
+
+    public String toString(){
+        return "Album{" + "uniqeId=" + uniqeId + ", name='" + name + '\'' + '}' ;
+    }
+
+}
