903d07adba27d0bdc909c76c609b17a7986c0924
Elnazt27 <151857047+Elnazt27@users.noreply.github.com>
2024-03-31 16:28:42 +0330

    c11 Artist class.

---
+++ codeSourse/codeSourse/src/model/AcountUser/Artist/Artist.java
@@ -0,0 +1,55 @@
+package model.AcountUser.Artist;
+
+import model.AcountUser.AccountUser;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class Artist extends AccountUser {
+    private double income;
+    private List<AccountUser> followers;
+    private String biography;
+    //*********************************************
+    public Artist(String userName, String password, String fullName, String email, String phoneNumber, Date birthDate,double income,String biography,List<AccountUser> followers) {
+        super(userName, password, fullName, email, phoneNumber,birthDate);
+        this.income = income;
+        this.biography = biography;
+        this.followers = new ArrayList<>();
+    }
+    //*********************************************
+    public double getIncome() {
+        return income;
+    }
+
+    public void setIncome(double income) {
+        this.income = income;
+    }
+
+    public List<AccountUser> getFollowers() {
+        return followers;
+    }
+
+    public void setFollowers(List<AccountUser> followers) {
+        this.followers = followers;
+    }
+
+    public String getBiography() {
+        return biography;
+    }
+
+    public void setBiography(String biography) {
+        this.biography = biography;
+    }
+    //*********************************************
+    @Override
+    public String toString() {
+        return "Artist{" +
+                "userName='" + getUserName() + '\'' +
+                ", fullName='" + getFullName() + '\'' +
+                ", income=" + income +
+                ", followers=" + followers +
+                ", biography='" + biography + '\'' +
+                '}';
+    }
+}
