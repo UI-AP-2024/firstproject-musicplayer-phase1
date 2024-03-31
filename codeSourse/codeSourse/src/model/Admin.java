7ded340c69e4d69dae44c52c3a0fde8bfa845d40
Elnazt27 <151857047+Elnazt27@users.noreply.github.com>
2024-03-31 11:55:09 +0330

    c08 Admin class.

---
+++ codeSourse/codeSourse/src/model/Admin.java
@@ -0,0 +1,40 @@
+package model;
+
+import java.util.Date;
+
+public class Admin extends AccountUser {
+    private static Admin instance;
+
+    //*********************************************
+    private Admin(String userName, String password, String fullName, String email, String phoneNumber, Date birthDate) {
+        super(userName, password, fullName, email, phoneNumber, birthDate);
+    }
+    //*********************************************
+
+    public static Admin getInstance() {
+        if (instance == null) {
+            instance = new Admin("admin", "admin123", "Admin User", "admin@example.com", "1234567890", new Date());
+        }
+        return instance;
+    }
+    @Override
+    public String toString() {
+        return "Admin{" +
+                "username='" +
+                getUserName() + '\'' +
+                ", password='" +
+                getPassword() + '\'' +
+                ", fullName='" +
+                getFullName() + '\'' +
+                ", email='" +
+                getEmail() + '\'' +
+                ", phoneNumber='" +
+                getPhoneNumber() + '\'' +
+                ", birthDate="
+                + getBirthDate() +
+                '}';
+    }
+}
+
+
+
