4f7fa57a96884281b2cce078f7b0e284af820d61
Maryam Dar <maryamdar1384@gmail.com>
2024-03-31 22:10:41 +0330

    add listener sign up

--- mpproj/src/controller/ListenerController.java
+++ mpproj/src/controller/ListenerController.java
@@ -1,9 +1,13 @@
 package controller;
 
 import java.util.Date;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import model.database.Database;
+import model.user.FreeListener;
 import model.user.Listener;
+import model.user.User;
 
 public class ListenerController {
     private static ListenerController listenerController;
@@ -24,10 +28,45 @@ public class ListenerController {
         this.listener = listener;
     }
 
-    public void signupNewListener(String password,String username, String firstName, String lastName, String emailAddress, String phoneNumber,
-            Date birthDate, double accountCredit){
-        Listener tmp = new Listener(password,username, firstName, lastName, emailAddress, phoneNumber, birthDate, accountCredit);
+    public boolean emailPassRegex(String email){
+        Pattern pattern = Pattern.compile("^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$");
+        Matcher matcher = pattern.matcher(email);
+        return matcher.matches();
+    }
+    public boolean phoneNumPassRegex(String phoneNumber){
+        Pattern pattern = Pattern.compile("^09[0-9]{9}$");
+        Matcher matcher = pattern.matcher(phoneNumber);
+        return matcher.matches();
+    }
+    public boolean passwordPassRegex(String password){
+        Pattern pattern = Pattern.compile("^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$");
+        Matcher matcher = pattern.matcher(password);
+        return matcher.matches();
+    }
+    public boolean usernameIsUnic(String username){
+        for(User user : Database.getDatabase().getAllUsers()){
+            if(user.getUsername()==username)
+                return false;
+        }
+        return true;
+    }
+    public String signupNewListener(String password,String username, String firstName, String lastName, String emailAddress, String phoneNumber,
+            Date birthDate){
+        if(!(emailPassRegex(emailAddress))){
+            return"Please enter a valid email address";
+        }
+        if(!(phoneNumPassRegex(phoneNumber))){
+            return"Please enter a valid phone Number";
+        }
+        if(!(passwordPassRegex(password))){
+            return"Not a strong password!! Your password must be at least 8 characters long, contain at least one number and have a mixture of uppercase and lowercase letters.";
+        }
+        if(!(usernameIsUnic(username))){
+            return"This username is alredy taken ! Please enter another one";
+        }
+        FreeListener tmp = new FreeListener(password,username, firstName, lastName, emailAddress, phoneNumber, birthDate, 50);
         Database.getDatabase().addToAllUsers(tmp);
+        return "Thanks for signing up. Your account has been created";
     }
     
 }
