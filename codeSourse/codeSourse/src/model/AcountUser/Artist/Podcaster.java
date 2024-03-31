b818f4856305a433a07edbfaf198e486f53aa0f2
Elnazt27 <151857047+Elnazt27@users.noreply.github.com>
2024-03-31 16:40:33 +0330

    c11 Podcaster class.

---
+++ codeSourse/codeSourse/src/model/AcountUser/Artist/Podcaster.java
@@ -0,0 +1,34 @@
+package model.AcountUser.Artist;
+import model.AcountUser.AccountUser;
+
+import java.util.Date;
+import java.util.List;
+
+public class Podcaster extends Artist {
+    private List<String> podcasts;
+
+    //*********************************************
+    public Podcaster(String userName, String password, String fullName, String email, String phoneNumber, Date birthDate, double income, String biography, List<AccountUser> followers,List<String> podcasts) {
+        super(userName, password, fullName, email, phoneNumber,birthDate,income,biography,followers);
+        this.podcasts = podcasts;
+    }
+    //*********************************************
+    public List<String> getPodcasts() {
+        return podcasts;
+    }
+    public void setPodcasts(List<String> podcasts) {
+        this.podcasts = podcasts;
+    }
+    //*********************************************
+    @Override
+    public String toString() {
+        return "Podcaster{" +
+                "userName='" + getUserName() + '\'' +
+                ", fullName='" + getFullName() + '\'' +
+                ", income=" + getIncome() +
+                ", followers=" + getFollowers() +
+                ", biography='" + getBiography() + '\'' +
+                ", podcasts=" + podcasts +
+                '}';
+    }
+}
