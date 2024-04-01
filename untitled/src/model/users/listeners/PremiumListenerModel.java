4a928f9c086579a82cfbb4aef2fa1faadd9adef2
jeonharry <jeonharry666@gmail.com>
2024-04-01 12:21:03 +0330

    premiumlistener with tostring method

---
+++ untitled/src/model/users/listeners/PremiumListenerModel.java
@@ -0,0 +1,22 @@
+package model.users.listeners;
+
+public class PremiumListenerModel extends ListenerModel
+{
+    private int remainingDays;
+    public PremiumListenerModel(String userName, String password, String fullName, String email, String phoneNumber, String birthDate)
+    {
+        super(userName,password,fullName,email,phoneNumber,birthDate);
+    }
+    public void setRemainingDays(int remainingDays)
+    {
+        this.remainingDays=remainingDays;
+    }
+    public int getRemainingDays()
+    {
+        return this.remainingDays;
+    }
+    public String toString()
+    {
+        return super.toString()+"Remaining subscription days: "+this.remainingDays;
+    }
+}
