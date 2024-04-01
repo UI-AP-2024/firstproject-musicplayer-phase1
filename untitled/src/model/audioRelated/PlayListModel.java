67e9dde8e9836682ec54a84e0a504ab94b478092
jeonharry <jeonharry666@gmail.com>
2024-04-01 12:10:59 +0330

    playlistModel class with tostring methid

---
+++ untitled/src/model/audioRelated/PlayListModel.java
@@ -0,0 +1,57 @@
+package model.audioRelated;
+
+import java.util.ArrayList;
+
+public class PlayListModel
+{
+    private long playListID;
+    private String playListName;
+    private String creator;
+    private ArrayList <AudioModel> audios=new ArrayList<>();
+    public PlayListModel(String playListName, String creator)
+    {
+        this.playListName=playListName;
+        this.creator=creator;
+    }
+    public void setPlayListID(long playListID)
+    {
+        this.playListID=playListID;
+    }
+    public void setPlayListName(String playListName)
+    {
+        this.playListName = playListName;
+    }
+    public void setCreator(String creator)
+    {
+        this.creator = creator;
+    }
+    public void setAudios(ArrayList<AudioModel> audios)
+    {
+        this.audios = audios;
+    }
+    public long getPlayListID()
+    {
+        return playListID;
+    }
+    public String getPlayListName()
+    {
+        return playListName;
+    }
+    public String getCreator()
+    {
+        return creator;
+    }
+    public ArrayList<AudioModel> getAudios()
+    {
+        return audios;
+    }
+    public String toString ()
+    {
+        String string="Playlist name: "+this.playListName+"\nPlaylist ID: "+this.playListID+"\nCreator's name: "+this.creator;
+        StringBuilder temp=new StringBuilder("\nAudios:\n");
+        for(AudioModel audioTemp:this.audios)
+            if(audioTemp!=null)
+                temp.append(audioTemp).append("\n");
+        return string+temp;
+    }
+}
