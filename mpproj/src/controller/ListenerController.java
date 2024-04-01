f4d2af627e422427e826a8159fee1fef9737019a
mohsen <im.mohsen.369@gmail.com>
2024-04-01 16:05:18 +0430

    start the controller

--- mpproj/src/controller/ListenerController.java
+++ mpproj/src/controller/ListenerController.java
@@ -1,5 +1,24 @@
 package controller;
 
-public class ListenerController {
+import model.UserAccount.Listener.ListenerModel;
+import model.UserAccount.UserAccountModel;
 
+import java.util.regex.Pattern;
+
+public class ListenerController {
+        private ListenerModel listener;
+        public ListenerModel getListener() {
+                return listener;
+        }
+        public void setListener(ListenerModel listener) {
+                this.listener = listener;
+        }
+
+        private static ListenerController listenerController;
+
+        public static ListenerController getListenerController() {
+                if (listenerController == null)
+                        listenerController = new ListenerController();
+                return listenerController;
+        }
 }
