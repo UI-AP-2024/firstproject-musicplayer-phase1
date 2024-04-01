c10cec32057a327f289c26dcd1857e21e3028d47
jeonharry <jeonharry666@gmail.com>
2024-04-01 12:09:49 +0330

    albumModel class with tostring method

---
+++ untitled/src/model/audioRelated/AlbumModel.java
@@ -0,0 +1,57 @@
+package model.audioRelated;
+
+import java.util.ArrayList;
+
+public class AlbumModel
+{
+    private long albumID;
+    private String albumName;
+    private String nameOfArtist;
+    private ArrayList <MusicModel> musics=new ArrayList<>();
+    public AlbumModel(String albumName, String nameOfArtist)
+    {
+        this.albumName=albumName;
+        this.nameOfArtist=nameOfArtist;
+    }
+    public void setAlbumID(long albumID)
+    {
+        this.albumID=albumID;
+    }
+    public void setAlbumName(String albumName)
+    {
+        this.albumName=albumName;
+    }
+    public void setNameOfArtist(String nameOfArtist)
+    {
+        this.nameOfArtist = nameOfArtist;
+    }
+    public void setMusics(ArrayList<MusicModel> musics)
+    {
+        this.musics = musics;
+    }
+    public long getAlbumID()
+    {
+        return this.albumID;
+    }
+    public ArrayList<MusicModel> getMusics()
+    {
+        return musics;
+    }
+    public String getAlbumName()
+    {
+        return albumName;
+    }
+    public String getNameOfArtist()
+    {
+        return this.nameOfArtist;
+    }
+    public String toString()
+    {
+        String string="Album name: "+this.albumName+"\nAlbum ID: "+this.albumID+"\nAlbum's artist: "+this.nameOfArtist;
+        StringBuilder temp=new StringBuilder("\nMusics:\n");
+        for(MusicModel musicTemp:this.musics)
+            if(musicTemp!=null)
+                temp.append(musicTemp).append("\n");
+        return string+temp;
+    }
+}
