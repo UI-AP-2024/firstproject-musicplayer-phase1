3f1d357f7d5fcf482ca24fed414da7713ade35e2
jeonharry <jeonharry666@gmail.com>
2024-04-01 13:34:11 +0330

    add following property to listenerModel

--- untitled/src/model/users/listeners/ListenerModel.java
+++ untitled/src/model/users/listeners/ListenerModel.java
@@ -4,6 +4,7 @@ import model.audioRelated.AudioModel;
 import model.audioRelated.Genre;
 import model.audioRelated.PlayListModel;
 import model.users.AccountUserModel;
+import model.users.artists.ArtistModel;
 
 import java.util.ArrayList;
 import java.util.Calendar;
@@ -17,6 +18,7 @@ public class ListenerModel extends AccountUserModel
     private Map <AudioModel,Long> playingAmount=new HashMap<>();
     private Calendar subscriptionExpiration;
     private ArrayList <Genre> favGenres=new ArrayList<>();
+    private ArrayList <ArtistModel> followings=new ArrayList<>();
     public ListenerModel(String userName, String password, String fullName, String email, String phoneNumber, String birthDate)
     {
         super(userName,password,fullName,email,phoneNumber,birthDate);
@@ -42,6 +44,14 @@ public class ListenerModel extends AccountUserModel
     public void setFavGenres(ArrayList<Genre> favGenres)
     {
         this.favGenres = favGenres;
+    }
+    public void setFollowings(ArrayList<ArtistModel> followings)
+    {
+        this.followings = followings;
+    }
+    public ArrayList<ArtistModel> getFollowings()
+    {
+        return this.followings;
     }
     public double getListenerCredit()
     {
