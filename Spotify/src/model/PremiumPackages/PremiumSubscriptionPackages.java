0b6d9446292780f04997d89bf86518c64992a5be
AliMirlohi <ali84mirloho@gmail.com>
2024-04-02 17:34:27 +0330

    complet PremiumSubscriptionPackages.java;

--- Spotify/src/model/PremiumPackages/PremiumSubscriptionPackages.java
+++ Spotify/src/model/PremiumPackages/PremiumSubscriptionPackages.java
@@ -1,5 +1,15 @@
 package model.PremiumPackages;
 
 public enum PremiumSubscriptionPackages {
-    THIRTYDAYS,SIXTYDAYS,ONEHUNDREDEIGHTYDAYS
+    THIRTYDAYS(5),SIXTYDAYS(9),ONEHUNDREDEIGHTYDAYS(14);
+
+    private final int value;
+    private PremiumSubscriptionPackages(int value){
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
 }
