7a2508fdb414642810d932be3a295779c88e62f2
Elnazt27 <151857047+Elnazt27@users.noreply.github.com>
2024-04-02 19:25:12 +0330

    c26 displayArtistFollowers & displayPlayCount classes. .

---
+++ codeSourse/codeSourse/src/controller/ArtistC.java
@@ -0,0 +1,40 @@
+package controller;
+
+import model.AccountUser.AccountUser;
+import model.AccountUser.Artist.Artist;
+import model.Audio.Audio;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class ArtistC {
+          public void displayArtistFollowers(Artist artist) {
+            List<AccountUser> followers = artist.getFollowers();
+            if (followers.isEmpty()) {
+                System.out.println("This artist has no followers.");
+            } else {
+                System.out.println("Followers of " + artist.getFullName() + ":");
+                for (AccountUser follower : followers) {
+                    System.out.println(follower.getFullName());
+                }
+            }
+        }
+
+        public void displayPlayCount(List<Audio> audios) {
+            Map<String, Integer> playCountMap = new HashMap<>();
+
+            for (Audio audio : audios) {
+                String title = audio.getTitle();
+                int playCount = audio.getPlayCount();
+                playCountMap.put(title, playCount);
+            }
+
+            // Display play count for each audio
+            System.out.println("Play count for each audio:");
+            for (Map.Entry<String, Integer> entry : playCountMap.entrySet()) {
+                System.out.println(entry.getKey() + ": " + entry.getValue());
+            }
+        }
+    }
+
