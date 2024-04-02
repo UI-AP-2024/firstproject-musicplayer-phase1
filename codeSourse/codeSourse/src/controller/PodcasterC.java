273ac28bf1845f3ea70ded1f93dea7d2b91b2175
Elnazt27 <151857047+Elnazt27@users.noreply.github.com>
2024-04-02 20:22:52 +0330

    c28 PodcasterC. .

---
+++ codeSourse/codeSourse/src/controller/PodcasterC.java
@@ -0,0 +1,22 @@
+package controller;
+
+import model.AccountUser.Artist.TypeOfArtist.Podcaster;
+import model.Audio.Audio;
+import model.Genre;
+
+import java.util.Date;
+import java.util.List;
+
+public class PodcasterC {
+
+
+    public void publishPodcast(Podcaster podcaster, int uniqueId, String title, int playCount, int likes, Date releaseDate, Genre genre, String audioLink, String cover) {
+        Audio podcast = new Audio(uniqueId, title, podcaster.getFullName(), playCount, likes, releaseDate, genre, audioLink, cover);
+
+        podcaster.getPodcasts().add(title);
+
+        System.out.println("Podcast '" + title + "' published successfully by " + podcaster.getFullName() + ".");
+    }
+}
+
+
