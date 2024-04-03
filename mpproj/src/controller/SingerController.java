d39aa2b9abd9ddb77296826266e5ec45488acbf3
Alireza <aliharadi10@gmail.com>
2024-04-04 02:16:38 +0330

    Creating publishMusic method in the SingerController class

--- mpproj/src/controller/SingerController.java
+++ mpproj/src/controller/SingerController.java
@@ -1,9 +1,9 @@
 package controller;
 
-import model.Album;
-import model.Premium;
-import model.Singer;
-import model.UserAccount;
+import model.*;
+
+import javax.xml.crypto.Data;
+import java.util.Date;
 
 public class SingerController {
 
@@ -34,19 +34,18 @@ public class SingerController {
         ((Singer) getUserAccount()).getAlbumList().add(album);
         return "Album successfully added";
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    public String publishMusic(String name, String gener,String musicText,String link,String cover,long albumId){
+        Audio.setCountId(Audio.getCountId()+1);
+        Date date = new Date();
+        Gener gener1 = Gener.valueOf(gener);
+        Music music = new Music(Audio.getCountId(),name,getUserAccount().getName(),date,gener1,link,cover,musicText);
+        Database.getDataBase().getAudio().add(music);
+        for ( Album album : ((Singer) getUserAccount()).getAlbumList() ){
+            if ( album.getId() == albumId ){
+                album.getMusicList().add(music);
+                break;
+            }
+        }
+        return "Podcast successfully added";
+    }
 }
