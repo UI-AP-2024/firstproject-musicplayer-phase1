0af0fed06bb8398a75d03a442359ec666aa21262
mohammad <mohammadjamshidi364@gmail.com>
2024-04-03 23:09:50 +0330

    change Main

--- mpproj/src/Main.java
+++ mpproj/src/Main.java
@@ -1,6 +1,17 @@
+
+import Model.Accounts.AdminModel;
+import Model.Accounts.Artists.ArtistModel;
+import Model.Audios.MusicModel;
+import Model.Database.Database;
+import View.MainView;
+
+import java.util.Date;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hi there :)");
-        // comment
+
+        int[] date = new int[3]; date[0] = 2000; date[1] = 1 ; date[2] = 1;
+        AdminModel.getAdmin("Admin" , "Admin1234" , "Admin" , "admin@gmail.com" , "09111111111" , date);
+        System.out.println(MainView.getMainView().mainMenu(""));
     }
 }
