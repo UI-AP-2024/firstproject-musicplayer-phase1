72b702f2fa925451d58cffde9d114a44d1649e80
mohammad <mohammadjamshidi364@gmail.com>
2024-04-04 01:06:47 +0330

    .

--- mpproj/src/Main.java
+++ mpproj/src/Main.java
@@ -13,5 +13,6 @@ public class Main {
         int[] date = new int[3]; date[0] = 2000; date[1] = 1 ; date[2] = 1;
         AdminModel.getAdmin("Admin" , "Admin1234" , "Admin" , "admin@gmail.com" , "09111111111" , date);
         System.out.println(MainView.getMainView().mainMenu(""));
+        
     }
 }
