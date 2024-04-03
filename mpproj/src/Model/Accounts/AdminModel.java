0255d6b240f8f298c3089f16a36208f73d37f667
mohammad <mohammadjamshidi364@gmail.com>
2024-04-03 23:14:11 +0330

    fix AdminModel

--- mpproj/src/Model/Accounts/AdminModel.java
+++ mpproj/src/Model/Accounts/AdminModel.java
@@ -1,23 +1,26 @@
 package Model.Accounts;
 
+import Model.Database.Database;
+
 import java.util.Date;
 
 public class AdminModel extends AccountsModel {
 
-    private AdminModel admin;
+    private static AdminModel admin;
     private AdminModel(String username , String password , String fullName , String email , String phoneNumber , int[] birthDate){
         super(username , password , fullName , email , phoneNumber , birthDate);
+
     }
 
-    public AdminModel getAdmin(String username , String password , String fullName , String email , String phoneNumber , int[] birthDate){
+    public static AdminModel getAdmin(String username , String password , String fullName , String email , String phoneNumber , int[] birthDate){
         if(admin == null){
             admin = new AdminModel(username , password , fullName , email , phoneNumber , birthDate);
+            Database.getDatabase().addAccount(admin);
         }
-
         return admin;
     }
 
     public AdminModel getAdmin(){
-        return this.admin;
+        return admin;
     }
 }
