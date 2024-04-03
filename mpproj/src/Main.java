0397a316117e39387a67707fbb64eaee966a83b3
Pooneh Mirfatah <poonemirfatah@gmail.com>
2024-04-03 15:54:40 +0330

    Main

--- mpproj/src/Main.java
+++ mpproj/src/Main.java
@@ -1,6 +1,16 @@
+import Controller.ListenerController;
+import Model.Admin;
+import Model.DataBase;
+import Model.Listener;
+import view.AdminView;
+import view.ListenerView;
+import view.View;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hi there :)");
-        // comment
+        Admin.getAdmin("Amir","1234","AmirAliGoli",
+                "amir@gmail.com","09133214546",1379,3,5);
+        View.firstPage();
+
     }
 }
