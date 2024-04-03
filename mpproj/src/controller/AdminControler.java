bee469da606d1ec28167ec824841a4c6bb8265a9
sadrabarani <sadra86barani@gmail.com>
2024-04-03 14:37:20 +0330

    Admin compelete regex added

--- mpproj/src/controller/AdminControler.java
+++ mpproj/src/controller/AdminControler.java
@@ -8,7 +8,7 @@ import model.UserAccount.*;
 import java.util.ArrayList;
 import java.util.Date;
 
-public class AdminControler {//todo admin controler
+public class AdminControler {
     Admin admin;
     private static AdminControler adminControler;
 
@@ -29,7 +29,13 @@ public class AdminControler {//todo admin controler
         return admin;
     }
 
-    public String signUpArtist(String username, String pasword, String name, String email, String phoneNum, Date birthDate, String bio, String type){
+    public String signUpAdmin(String username, String pasword, String name, String email, String phoneNum, Date birthDate){
+        if(!Regex.emailRegex(email))
+            return "use valid email";
+        if (!Regex.passwordRegex(pasword))
+            return "use harder password";
+        if(!Regex.phoneRegex(phoneNum))
+            return "use valid phone number";
         ArrayList<User> users= Database.getDatabase().getUsers();
         for(User user:users){
             if(user.getUsername().equals(username)){
@@ -39,7 +45,6 @@ public class AdminControler {//todo admin controler
         admin = Admin.getAdmin(username, pasword, name, email, phoneNum, birthDate);
         setAdmin(admin);
         Database.getDatabase().getUsers().add((Admin)admin);
-        //todo:add to database
         admin.setIsLogin(true);
         return  "artist accaount successfully created .";
     }
@@ -82,6 +87,47 @@ public class AdminControler {//todo admin controler
         StringBuilder result=new StringBuilder("audios :\n");
         for(Audio audio:Database.getDatabase().getAudios()) {
             result.append(audio.getTitle()).append(", ");
+        }
+        return String.valueOf(result);
+    }
+    public String lookAudioInfo(int audioId){
+        for(Audio audio:Database.getDatabase().getAudios()) {
+            if(audio.getId()==audioId){
+                return "artist name : "+audio.getArtistName()+
+                        "title"+audio.getTitle() +
+                        "play count "+audio.getPlayCount()+
+                        "file link"+audio.getAudioFileLink()+
+                        "likes : "+audio.getLikes();
+            }
+        }
+        return "error : not found";
+    }
+    public String lookArtistInfo(String username){
+        for(User user:Database.getDatabase().getUsers()){
+            if(username.equals(user.getUsername())){
+                return  "user name"+user.getUsername()+
+                        " email: "+user.getEmail()+
+                        " full namme : "+user.getFullName()+
+                        " phone number ;"+user.getPhoneNumber();
+            }
+        }
+        return "error : not found";
+    }
+    public String lookMostLikeAudio(){
+        ArrayList<Audio> audioArrayList=Database.getDatabase().getAudios();
+        Audio tmpAudio;
+        for (int i = 0; i < audioArrayList.size()-1; i++) {
+            for (int j = 0; j < audioArrayList.size()-1-i; j++) {
+                if(audioArrayList.get(j).getLikes()<audioArrayList.get(j+1).getLikes()){
+                    tmpAudio=audioArrayList.get(j);
+                    audioArrayList.set(j,audioArrayList.get(j+1));
+                    audioArrayList.set(j+1,tmpAudio);
+                }
+            }
+        }
+        StringBuilder result=new StringBuilder("audios :\n");
+        for(Audio audio:audioArrayList) {
+            result.append(audio.getTitle()).append(", ").append("likes : ").append(audio.getLikes()).append("\n");
         }
         return String.valueOf(result);
     }
--- mpproj/src/view/AdminViewer.java
+++ mpproj/src/view/AdminViewer.java
@@ -1,4 +1,70 @@
 package view;
 
-public class AdminViewer {//todo admin viewer
+import controller.AdminControler;
+
+import java.util.Scanner;
+
+public class AdminViewer {
+    private static AdminViewer adminViewer;
+
+    Scanner sc = new Scanner(System.in);
+    private String answer;
+    String[] answers;
+
+    public AdminViewer() {
+    }
+
+    public static AdminViewer getAdminViewer() {
+        if (adminViewer == null)
+            adminViewer = new AdminViewer();
+        return adminViewer;
+    }
+
+    public void getAnswer() {
+        answer = sc.nextLine();
+        answers = answer.split("-");
+        if (answers[0].equals("Logout")) {
+            logout();
+        } else if (answers[0].equals("Artists")) {
+
+        } else if (answers[0].equals("Audios")) {
+
+        } else if (answers[0].equals("Audio")) {
+
+        } else if (answers[0].equals("Artist")) {
+
+        } else if (answers[0].equals("Reports")) {
+
+        } else {
+            print("wrong command");
+            getAnswer();
+        }
+    }
+    public void audioInfo(){
+        print(AdminControler.getAdminControler().lookAudioInfo(Integer.parseInt(answers[1])));
+        getAnswer();
+    }
+    public void audios(){
+        print(AdminControler.getAdminControler().lookAudios());
+        getAnswer();
+    }
+    public void reports(){
+        print(AdminControler.getAdminControler().lookReport());
+        getAnswer();
+    }
+    public void artist(){
+        print(AdminControler.getAdminControler().lookArtistInfo(answers[1]));
+        getAnswer();
+    }
+    public void artists(){
+        print(AdminControler.getAdminControler().lookArtists());
+        getAnswer();
+    }
+    public void logout(){
+        print(AdminControler.getAdminControler().logout());
+        MainViewer.getMainViewer().getanswer();
+    }
+    private void print (Object object){
+        System.out.println(object);
+    }
 }
