d82454a9dc59eac49099f0f7e27dcacb4346afdf
mohammad <mohammadjamshidi364@gmail.com>
2024-04-03 23:15:08 +0330

    fix SingerModel

--- mpproj/src/Model/Accounts/Artists/SingerModel.java
+++ mpproj/src/Model/Accounts/Artists/SingerModel.java
@@ -1,29 +1,30 @@
 package Model.Accounts.Artists;
 
 import Model.Audios.AlbumModel;
+import Model.Audios.AudioModel;
 import Model.Audios.MusicModel;
 
 import java.util.ArrayList;
 import java.util.Date;
 
 public class SingerModel extends ArtistModel {
-    private ArrayList<MusicModel> musicList;
+    private ArrayList<AudioModel> musicList;
 
     private ArrayList<AlbumModel> albumList;
 
     public SingerModel(String username , String password , String fullName , String email , String phoneNumber , int[] birthDate , String bio) {
         super(username, password, fullName, email, phoneNumber, birthDate , bio);
 
-        this.musicList = new ArrayList<MusicModel>();
+        this.musicList = new ArrayList<AudioModel>();
         this.albumList = new ArrayList<AlbumModel>();
     }
 
 
-    public ArrayList<MusicModel> getMusicList(){
+    public ArrayList<AudioModel> getMusicList(){
         return this.musicList;
     }
 
-    public void AddMusic(MusicModel music){
+    public void addMusic(MusicModel music){
         this.musicList.add(music);
     }
 
