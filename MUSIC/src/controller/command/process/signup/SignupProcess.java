56a3f675297678d272eb8116ebe93f2c790f7695
Taha <126871688+TahaYGII@users.noreply.github.com>
2024-04-04 05:11:48 +0330

    change pack move All signup package class as controller command process package

---
+++ MUSIC/src/controller/command/process/signup/SignupProcess.java
@@ -0,0 +1,31 @@
+package controller.command.process.signup;
+
+import controller.command.process.Process;
+import model.enums.ProcessName;
+import model.exception.ProcessException;
+
+public class SignupProcess implements Process {
+
+    @Override
+    public String exec(String[] commands) throws ProcessException {
+
+        if (commands.length > 11) {
+            throw new ProcessException(this, "Invalid param");
+        }
+
+        final String listenerType = commands[1];
+
+        switch (getValue(listenerType)) {
+            case "L":
+                return ProcessName.SIGNUP_LISTENER.process.exec(commands);
+            case "S":
+                return ProcessName.SIGNUP_SINGER.process.exec(commands);
+            case "P":
+                return "NOT";
+            default:
+                return "";
+        }
+
+    }
+
+}
